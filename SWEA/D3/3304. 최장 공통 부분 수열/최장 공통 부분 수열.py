c2f5acd493a998d1a0fe7dfaf20c12757aacaa70
leunseongl <105471465+leunseongl@users.noreply.github.com>
2022-11-06 00:56:32 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 350 ms, Memory: 91,544 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
@@ -1,15 +1,12 @@
 t = int(input())
-
-for tc in range(1, t + 1) :
+for tc in range(1, t+1):
     a, b = input().split()
-    dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1)]
-
-    for i in range(1, len(dp)) :
-        for j in range(1, len(dp[i])) :
-            # 같을 경우
-            if a[j-1] == b[i-1] :
-                dp[i][j] = dp[i-1][j-1] + 1
-            else : # 다를 경우
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-    print('#%d %d' % (tc, dp[-1][-1]))
+    n, m = len(a), len(b)
+    g = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if b[i-1] == a[j-1]:
+                g[i][j] = g[i-1][j-1]+1
+            else:
+                g[i][j] = max(g[i-1][j], g[i][j-1])
+    print('#%d' %tc, g[-1][-1])
