22721636388224fb9ae4f7c158fa60100c143bd2
leunseongl <105471465+leunseongl@users.noreply.github.com>
2022-11-09 22:41:28 +0900

    [D3] Title: USB 꽂기의 미스터리, Time: 129 ms, Memory: 58,468 KB -BaekjoonHub

---
+++ SWEA/D3/9700. USB 꽂기의 미스터리/USB 꽂기의 미스터리.py
@@ -0,0 +1,9 @@
+t = int(input())
+for tc in range(1, t+1):
+    p, q = map(float, input().split())
+    s1 = (1-p)*q
+    s2 = p*(1-q)*q
+    if s1<s2:
+        print('#%d' %tc, 'YES')
+    else:
+        print('#%d' %tc, 'NO')
