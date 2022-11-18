a61f2c4dd381be9daa3f10ef14703cfe67c55421
leunseongl <105471465+leunseongl@users.noreply.github.com>
2022-11-18 20:10:25 +0900

    [Silver V] Title: 거스름돈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,13 +1,14 @@
 n = int(input())
-dp = [-1] * (n+8)
-dp[2] = 1
-dp[4] = 2
-dp[5] = 1
-dp[6] = 3
-dp[7] = 2
-dp[8] = 4
+res = 0
+while n:
+    if n%5 == 0:
+        res += n//5
+        break
+    else:
+        n -= 2
+        res += 1
 
-for i in range(9, n+1):
-    dp[i] = min(dp[i-2], dp[i-5])+1
-
-print(dp[n])
+if n<0:
+    print(-1)
+else:
+    print(res)
