19fb481e36514e1c74c4e72f07f25232233066f0
leunseongl <105471465+leunseongl@users.noreply.github.com>
2022-11-20 09:58:17 +0900

    [D3] Title: 최장 경로, Time: 169 ms, Memory: 62,836 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,28 +1,27 @@
 def DFS(x, cnt):
     global res
     res = max(res, cnt)
-
-    for j in g[x]:
-        if visit[j] == 0:
-            visit[j] = 1
-            DFS(j, cnt+1)
-            visit[j] = 0
+    for k in g[x]:
+        if visit[k] == 0:
+            visit[k] = 1
+            DFS(k, cnt+1)
+            visit[k] = 0
 
 t = int(input())
 for tc in range(1, t+1):
     n, m = map(int, input().split())
-    g = [[] for _ in range(n+1)]
-    visit = [0] * (n+1)
-    res = 0
-
+    g = [[]*(n+1) for _ in range(n+1)]
     for _ in range(m):
-        a,b = map(int, input().split())
+        a, b = map(int, input().split())
         g[a].append(b)
         g[b].append(a)
-
+    res = 0
+    visit = [0] * (n+1)
     for i in range(1, n+1):
-        visit[i] = 1
-        DFS(i, 1)
-        visit[i] = 0
+        if visit[i] == 0:
+            visit[i] = 1
+            DFS(i, 1)
+            visit[i] = 0
+
 
     print('#%d' %tc, res)
