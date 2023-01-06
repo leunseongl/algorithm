0705699c0101f8d1b6aa15c8be0445daf426c619
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-01-06 21:52:40 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 203 ms, Memory: 62,768 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,28 +1,48 @@
-def tot(li):
-    global res
-    sum1 = sum2 = 0
-    for i in range(100):
-        for j in range(100):
-            sum1 += li[i][j]
-            sum2 += li[j][i]
-        if sum1>res:
-            res = sum1
-        if sum2>res:
-            res = sum2
-        sum1 = sum2 = 0
-    sum1 = sum2 = 0
-    for i in range(100):
-        sum1 += li[i][i]
-        sum2 += li[100-i-1][i]
-    if sum1 > res:
-        res = sum1
-    if sum2 > res:
-        res = sum2
-    return res
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Solution {
 
-for _ in range(10):
-    tc = int(input())
-    li = [list(map(int, input().split())) for _ in range(100)]
-    res = 0
-    print('#%d' %tc, tot(li))
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int tc = 0; tc < 10; tc++) {
+			int big = 0;
+			int t = Integer.parseInt(br.readLine());
+			int[][] arr = new int[100][100];
+			for (int i = 0; i < 100; i++) {
+				String[] tmp = br.readLine().split(" ");
+				for (int j = 0; j < 100; j++) {
+					arr[i][j] = Integer.parseInt(tmp[j]);
+				}
+			}
+
+			for (int i = 0; i < 100; i++) {
+				int sum1 = 0;
+				int sum2 = 0;
+				for (int j = 0; j < 100; j++) {
+					sum1 += arr[i][j];
+					sum2 += arr[j][i];
+				}
+				if (sum1 > big)
+					big = sum1;
+				if (sum2 > big)
+					big = sum2;
+			}
+			int sum1 = 0;
+			int sum2 = 0;
+			for (int i = 0; i < 100; i++) {
+				sum1 += arr[i][i];
+				sum2 += arr[i][99 - i];
+			}
+			if (sum1 > big)
+				big = sum1;
+			if (sum2 > big)
+				big = sum2;
+			System.out.println("#" + t + " " + big);
+		}
+
+	}
+
+}
