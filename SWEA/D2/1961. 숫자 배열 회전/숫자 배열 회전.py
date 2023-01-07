65ed58586c0ca2efc1aa093976b71c55c8e4b30e
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-01-07 21:53:57 +0900

    [D2] Title: 숫자 배열 회전, Time: 138 ms, Memory: 58,624 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,32 +1,67 @@
-t = int (input())
-for tc in range(1, t+1):
-    n = int(input())
-    g = [list(map(int, input().split())) for _ in range(n)]
-    li_90 = [[0]*n for _ in range(n)]
-    li_180 = [[0]*n for _ in range(n)]
-    li_270 = [[0]*n for _ in range(n)]
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    for i in range(n):
-        for j in range(n):
-            li_90[i][j] = g[n-j-1][i]
+public class Solution {
 
-    for i in range(n):
-        for j in range(n):
-            li_180[i][j] = g[n-i-1][n-j-1]
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int tc = 0; tc<t; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			int[][] arr = new int[n][n]; //input 받을 배열
+			
+			//입력 받아 2차원 배열에 넣어주기
+			for(int i = 0; i<n; i++) {
+				String[] tmp = br.readLine().split(" ");
+				for(int j = 0; j<n; j++) {
+					arr[i][j] = Integer.parseInt(tmp[j]);
+				}
+			}
+			
+			//90도 회전
+			int[][] arr_90 = new int[n][n];
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<n; j++) {
+					arr_90[i][j] = arr[n-j-1][i];
+				}
+			}
+			
+			//180도 회전
+			int[][] arr_180 = new int[n][n];
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<n; j++) {
+					arr_180[i][j] = arr[n-i-1][n-j-1];
+				} 
+			}
+			
+			//270도 회전
+			int[][] arr_270 = new int[n][n];
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<n; j++) {
+					arr_270[i][j] = arr[j][n-i-1];
+				} 
+			}
+			
+			//출력
+			System.out.println("#" + (tc+1));
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<n; j++) {
+					System.out.print(arr_90[i][j]);
+				}
+				System.out.print(" ");
+				for(int m = 0; m<n; m++) {
+					System.out.print(arr_180[i][m]);
+				}
+				System.out.print(" ");
+				for(int k = 0; k<n; k++) {
+					System.out.print(arr_270[i][k]);
+				}
+				System.out.println();
+			}
+		}
+		
+	}
 
-    for i in range(n):
-        for j in range(n):
-            li_270[i][j] = g[j][n-i-1]
-
-    #출력
-    print('#%d' %tc)
-    for i in range(n):
-        for j in range(n):
-            print(li_90[i][j], end='')
-        print(end=' ')
-        for m in range(n):
-            print(li_180[i][m], end='')
-        print(end=' ')
-        for k in range(n):
-            print(li_270[i][k], end='')
-        print()
+}
