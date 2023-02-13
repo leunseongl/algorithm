bb082580a3cf6660de744a913c3223c60ae77149
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-02-13 22:50:13 +0900

    [Gold V] Title: 탑, Time: 3372 ms, Memory: 135240 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+//1. 스택이 비면 넣는다, idx_stack도 같이 넣는다
+//2. 스택.peek이 자기보다 작거나 같으면 작은애 pop하고 넣는다
+//3. 스택.peek이 자기보다 크면 res배열 처리 후 넣는다 
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> idx_stack = new Stack<>();
+		int[] res = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+//			System.out.println(stack);
+//			System.out.print("인덱스");
+//			System.out.println(idx_stack);
+			boolean ifPop = false;
+			int tmp = Integer.parseInt(st.nextToken());
+			if(stack.isEmpty()) { //스택이 비었을 때
+				stack.push(tmp);
+				idx_stack.push(i+1);
+			}
+			else if(stack.peek() <= tmp) { //스택.peek이 자기보다 같거나 작으면
+				while(!stack.isEmpty() && stack.peek()<tmp) { //스택이 비거나 스택.peek이 자기보다 클때까지 pop
+					stack.pop();
+					idx_stack.pop();
+					ifPop = true;
+				}
+				//pop한 후에 pop을 한 애들만 res문 따로 처리
+				if(ifPop && !stack.isEmpty()) {
+					res[i] = idx_stack.peek();
+				}
+		
+				stack.push(tmp);
+				idx_stack.push(i+1);	
+			}
+			else {
+				res[i] = idx_stack.peek();
+				stack.push(tmp);
+				idx_stack.push(i+1);
+				}
+						
+			}
+
+
+        for(int i = 0; i<N; i++){
+            System.out.print(res[i] + " ");
+        }
+				
+		
+		
+	}
+
+}
