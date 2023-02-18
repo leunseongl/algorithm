39721686a246dfe474305171e16b7d27fd9d791b
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-02-19 00:40:51 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1688 ms, Memory: 37968 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -3,76 +3,74 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//순서: 우하좌상
+//시작 위치 정하기
+//map[0][0] 값 빼놓기
+
 public class Main {
-	
-	static int N, M, R;
-	static int mid;
-	static int[][] arr;
+
+	static int[] dx = {0,1,0,-1};
+	static int[] dy = {1,0,-1,0};
+	static int[][] map; 
+	static int N;
+	static int M;
 	
 	public static void main(String[] args) throws IOException {
- 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N][M];
+		map = new int[N][M];
 		for(int i = 0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j<M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		mid = Math.min(N, M)/2;
 		
 		for(int i = 0; i<R; i++) {
 			rotate();
 		}
-
+		
 		for(int i = 0; i<N; i++) {
 			for(int j = 0; j<M; j++) {
-				System.out.print(arr[i][j] + " ");
+				System.out.print(map[i][j] + " ");
 			} System.out.println();
 		}
-
-		
 		
 	}
-	
+
 	private static void rotate() {
-		for(int start = 0; start<mid; start++) {
+		
+		int start = Math.min(N, M)/2;
+		for(int s = 0; s<start; s++) {
 			
-			int tmp = arr[start][start];
+			int x = s;
+			int y = s;
+			int dir = 0;
 			
-			int n = N-1-start;
-			int m = M-1-start;
+			int temp = map[x][y];
 			
-			//왼쪽
-			for(int i = start; i<m; i++) {
-				arr[start][i] = arr[start][i+1];
-			}
-						
-			//위쪽
-			for(int i = start; i<n; i++) {
-				arr[i][m] = arr[i+1][m];
-			}
-			
-			//오른쪽
-			for(int i = m; i>start; i--) {
-				arr[n][i] = arr[n][i-1];
-			}
-			
-			//아래쪽
-			for(int i = n; i>start; i--) {
-				arr[i][start] = arr[i-1][start];
+			while(dir<4) {
+				int nx = dx[dir] + x;
+				int ny = dy[dir] + y;
+				
+				if(nx>=s && nx<N-s && ny>=s && ny<M-s) {
+					map[x][y] = map[nx][ny];
+					
+					x = nx;
+					y = ny;
+				}
+				
+				else dir++;
 			}
 			
-			arr[start+1][start] = tmp;
-			
+			map[s+1][s] = temp;
 		}
+		
 	}
-
 }
