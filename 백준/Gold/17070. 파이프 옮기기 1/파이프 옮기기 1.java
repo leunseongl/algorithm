e478c3f74e3b98b91844be5048e2c9d4cf251bda
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-03-19 11:02:43 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 312 ms, Memory: 18292 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,21 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//0만 탐색 가능, 1은 벽
+/**
+ * 파이프는 2개의 연속된 칸을 차지
+ * 파이프는 벽이 아닌 빈 칸만 차지
+ * 파이프를 밀 수 있는 방향은 총 3가지 
+ * 세로에서는 세로, 대각선
+ * 가로에서는 가로, 대각선
+ * 대각선은 대각선, 가로, 세로
+ * DFS로 풀이
+ */
+
+//Q: visit 배열이 필요 없는 건 가로, 세로, 대각선만 가서?
 public class Main {
 
-	//가로, 세로, 대각선
-	static int[] dx = {0,1,1};
-	static int[] dy = {1,0,1};
+	//방향: 가로-세로-대각선
+	static int[] dx = {0, 1, 1};
+	static int[] dy = {1, 0, 1};
+	
 	static int N;
 	static int[][] map;
-	static int count = 0;
-	
+	static int res;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
@@ -26,42 +35,40 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		//입력 완료
 		
-		DFS(0, 1, 0);
-		System.out.println(count);
+		//시작 지점은 0,1
+		dfs(0, 1, 0);
+		System.out.println(res);
+		
 	}
 	
-	//현재 방향 매개변수 - 0: 가로 / 1: 세로 / 2: 대각선
-	private static void DFS(int x, int y, int dir) {
+	//0 - 가로, 1 - 세로, 2 - 대각선
+	private static void dfs(int x, int y, int dir) {
 		
 		if(x == N-1 && y == N-1) {
-			count++;
+			res++;
 			return;
 		}
 		
 		for(int i = 0; i<3; i++) {
-			//가로 -> 세로 안됨, 세로 -> 가로 안됨
-			if((dir == 0 && i == 1)||(dir == 1 && i == 0)) {
-				continue;
-			}
 			
-			int nx = x+dx[i];
-			int ny = y+dy[i];
+			//가로일 때 세로 못가고, 세로일 때 가로 못감
+			if(dir == 0 && i == 1) continue;
+			if(dir == 1 && i == 0) continue;
 			
-			if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny] != 1) {
-				
-				if(i == 2 && (map[x+1][y] == 1 || map[x][y+1] == 1)) {
-					continue;
+			int nx = dx[i] + x;
+			int ny = dy[i] + y;
+			
+			if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny] == 0) {
+				if(i == 2) {
+					if(map[nx-1][ny]==0 && map[nx][ny-1]==0) {
+						dfs(nx, ny, i);
+					}
 				}
-				
-				DFS(nx, ny, i);
+				else dfs(nx, ny, i);
 			}
-			
 		}
 		
 	}
-	
-	
 
 }
