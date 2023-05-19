9676a8ef5168d8a0d8b9088cb56f6ad7ea6105ed
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-05-19 11:29:40 +0900

    [Gold IV] Title: 게리맨더링, Time: 156 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -8,20 +8,15 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/* 부분집합, bfs */
-/**
- * 인덱스는 1부터 받아줄 필요가 없다
- * 각 구는 1부터 N까지지지만 간선 정보를 -1해서 받아주고 0부터 처리해주면 됨
- */
 public class Main {
 
 	static int N;
 	static int[] population;
 	static List<List<Integer>> graph = new ArrayList<>();
-	static boolean[] isSelected;
-	static int res = Integer.MAX_VALUE;
+	static boolean[] visit;
+	static int answer = Integer.MAX_VALUE;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
@@ -36,74 +31,84 @@ public class Main {
 			graph.add(new ArrayList<>());
 		}
 		
-		for(int i  = 0; i<N; i++) {
+		for(int i = 0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int cnt = Integer.parseInt(st.nextToken());
-			for(int j = 0; j<cnt; j++) {
+			int n = Integer.parseInt(st.nextToken());
+			for(int j = 0; j<n; j++) {
 				graph.get(i).add(Integer.parseInt(st.nextToken())-1);
 			}
+		
 		}
 		//입력 완료
 		
-		isSelected = new boolean[N];
+		visit = new boolean[N];
 		divide(0);
-		System.out.println(res == Integer.MAX_VALUE ? -1 : res);
 		
+		System.out.println(answer==Integer.MAX_VALUE?-1:answer);
 	}
+	
+	//부분 집합
+	private static void divide(int cnt) {
 
-	private static void divide(int idx) {
-		//기저 조건
-		if(idx == N) {
-			List<Integer> listA = new ArrayList<>();
-			List<Integer> listB = new ArrayList<>();
+		//부분집합 한 개 완성 시
+		if(cnt == N) {
+			List<Integer> listA = new ArrayList<Integer>();
+			List<Integer> listB = new ArrayList<Integer>();
+			
 			for(int i = 0; i<N; i++) {
-				if(isSelected[i]) listA.add(i);
-				else if(!isSelected[i]) listB.add(i);
+				if(visit[i]) listA.add(i);
+				else listB.add(i);
 			}
 			
-			//bfs 돌리기 전 몰빵된 곳 제거
 			if(listA.size() == 0 || listB.size() == 0) return;
 			
 			if(bfs(listA) && bfs(listB)) {
-				//최소값 연산
-				int sum = 0;
-				for(int a : listA) {
-					sum += population[a];
+				int one = 0;
+				for(int o : listA) {
+					one += population[o];
 				}
-				for(int b : listB) {
-					sum -= population[b];
+				
+				int two = 0;
+				for(int o : listB) {
+					two += population[o];
 				}
-				res = Math.min(res, Math.abs(sum));
+				
+				answer = answer>Math.abs(one-two)?Math.abs(one-two):answer;
 			}
+			
 			return;
 		}
 		
-		isSelected[idx] = true;
-		divide(idx+1);
+		visit[cnt] = true;
+		divide(cnt+1);
 		
-		isSelected[idx] = false;
-		divide(idx+1);
+		visit[cnt] = false;
+		divide(cnt+1);
 	}
 
 	private static boolean bfs(List<Integer> list) {
-		
+
 		boolean[] visit = new boolean[N];
+		visit[list.get(0)] = true;
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		q.offer(list.get(0));
+		
 		int count = 0;
 		
-		Queue<Integer> q = new ArrayDeque<>();
-		q.offer(list.get(0));
-		visit[list.get(0)] = true;
-		
 		while(!q.isEmpty()) {
-			int tmp = q.poll();
+			int cur = q.poll();
 			count++;
-			for(int o : graph.get(tmp)) {
+			
+			for(int o : graph.get(cur)) {
 				if(!visit[o] && list.contains(o)) {
 					visit[o] = true;
 					q.offer(o);
 				}
 			}
 		}
-		return count == list.size();
+		
+		if(count == list.size()) return true;
+		else return false;
 	}
+	
 }
