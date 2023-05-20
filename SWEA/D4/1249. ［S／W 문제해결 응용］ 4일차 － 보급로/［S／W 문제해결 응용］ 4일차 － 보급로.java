1975b936c55aedadf4b2ea6ccc6b3f8c6ca9b1a0
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-05-20 16:23:32 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 168 ms, Memory: 29,500 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -2,39 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-/* bfs */
-//출발지에서 도착지까지 가는 경로 중에 복구 시간이 가장 짧은 경로에 대한 총 복구 시간
-//최소의 최소 -> pq 사용
 public class Solution {
 
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,-1,0,1};
+	
 	static class Node implements Comparable<Node>{
-		int x, y, time;
-		public Node(int x, int y, int time) {
+		int x, y, num, time;
+		public Node(int x, int y, int num, int time) {
 			this.x = x;
 			this.y = y;
+			this.num = num;
 			this.time = time;
 		}
 		@Override
 		public int compareTo(Node o) {
-			return this.time - o.time;
+			return this.time-o.time;
+		}
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y + ", num=" + num + ", time=" + time + "]";
 		}
 	}
 	
-	static int[] dx = {-1,0,0,1};
-	static int[] dy = {0,-1,1,0};
-	
-	static int N;
+	static int N, answer;
 	static int[][] map;
 	static boolean[][] visit;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
+			
 			map = new int[N][N];
 			for(int i = 0; i<N; i++) {
 				String[] line = br.readLine().split("");
@@ -44,38 +46,40 @@ public class Solution {
 			}
 			//입력 완료
 			
+			answer = 0;
 			visit = new boolean[N][N];
-			int res = bfs(0,0,0); //시작지점은 어차피 0
+			bfs();
 			
-			System.out.printf("#%d %d%n", tc, res);
-			
+			System.out.printf("#%d %d%n", tc, answer);
 		}
-		
-		
 	}
-	
-	private static int bfs(int x, int y, int sum) {
-		
+
+	private static void bfs() {
+
+		visit[0][0] = true;
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(x, y, sum));
-		visit[x][y] = true;
+		pq.offer(new Node(0,0,0,0)); //시작, 끝 위치는 num이 0
 		
 		while(!pq.isEmpty()) {
 			Node cur = pq.poll();
+			//System.out.println(cur);
 			
-			if(cur.x == N-1 && cur.y == N-1) 
-				return cur.time;
+			if(cur.x == N-1 && cur.y == N-1) {
+				answer = cur.time;
+				return;
+			}
 			
-			for(int i = 0; i<4; i++) {
-				int nx = dx[i] + cur.x;
-				int ny = dy[i] + cur.y;
+			for(int k = 0; k<4; k++) {
+				int nx = cur.x + dx[k];
+				int ny = cur.y + dy[k];
 				
 				if(nx>=0 && nx<N && ny>=0 && ny<N && !visit[nx][ny]) {
-					pq.offer(new Node(nx, ny, cur.time+map[nx][ny]));
 					visit[nx][ny] = true;
+					pq.offer(new Node(nx, ny, map[nx][ny], cur.time+map[nx][ny]));
 				}
 			}
 		}
-		return -1;
+		
+		
 	}
 }
