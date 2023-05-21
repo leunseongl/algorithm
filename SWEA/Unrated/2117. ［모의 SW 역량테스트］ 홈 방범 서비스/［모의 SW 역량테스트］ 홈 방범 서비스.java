79713a8deb803bf5bb570e6b11a291a1523172e7
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-05-21 15:58:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 392 ms, Memory: 88,500 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -13,85 +13,82 @@ public class Solution {
 	static class Node {
 		int x, y;
 		public Node(int x, int y) {
-			this.x = x; 
+			this.x = x;
 			this.y = y;
 		}
 	}
 	
 	static int N, M, answer;
-	static int[][] map;
+	static int[][] city;
 	static boolean[][] visit;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int T = Integer.parseInt(br.readLine());
+		
 		for(int tc = 1; tc<=T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken()); //하나의 집이 지불할 수 있는 비용
 			
-			map = new int[N][N];
+			city = new int[N][N];
 			for(int i = 0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j = 0; j<N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					city[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			//입력 완료
 			
-			answer = Integer.MIN_VALUE;
+			answer = 0;
+			//모든 부분을 시작점을 bfs 돌리기
 			for(int i = 0; i<N; i++) {
 				for(int j = 0; j<N; j++) {
-					visit = new boolean[N][N];	
+					visit = new boolean[N][N];
 					bfs(i, j);
 				}
 			}
-			
 			System.out.printf("#%d %d%n", tc, answer);
 		}
 	}
 	
 	private static void bfs(int x, int y) {
 
-		int K = 1; 
+		visit[x][y] = true;
+		int K = 1;
+		int pay = K * K + (K - 1) * (K - 1);
 		int home = 0;
 		
-		//K=1 일 때 계산
-		int cost = K*K+(K-1)*(K-1); //비용 계산
-		if(map[x][y] == 1) home++; //현재 위치가 집이라면 +1
-		if(home*M-cost>=0) answer = Math.max(answer, home);
+		//K가 1일 때  계산
+		if(city[x][y] == 1) { //현재 시작 위치가 집이면
+			home++; //집 카운트 해주고
+			if(pay<=M) answer = Math.max(1, answer);
+		}
 		
 		Queue<Node> q = new ArrayDeque<>();
 		q.offer(new Node(x, y));
-		visit[x][y] = true;
 		
 		while(!q.isEmpty()) {
 			int size = q.size();
-			K++;
 			
-			while(size-->0) {
+			while(size-->0) { //레벨별 bfs 탐색, 현재 큐에 있는 애들 동시에 감
 				Node cur = q.poll();
-				//System.out.println(cur);
 				
 				for(int k = 0; k<4; k++) {
 					int nx = cur.x + dx[k];
 					int ny = cur.y + dy[k];
 					
+					//경계선과 방문 조건 체크
 					if(nx>=0 && nx<N && ny>=0 && ny<N && !visit[nx][ny]) {
+						if(city[nx][ny] == 1) home++;
 						visit[nx][ny] = true;
 						q.offer(new Node(nx, ny));
-						if(map[nx][ny] == 1) home++;
 					}
 				}
 			}
-			
-			cost = K*K+(K-1)*(K-1);
-			if(home*M-cost>=0) answer = Math.max(answer, home);
-			
-			if(K == N+1) return;
+			K++;
+			pay = K * K + (K - 1) * (K - 1); //운영 비용
+			if(pay <= home*M) answer = Math.max(answer, home);
 		}
-		
 	}
-
 }
