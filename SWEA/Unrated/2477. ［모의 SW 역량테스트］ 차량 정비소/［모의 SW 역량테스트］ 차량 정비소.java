96662075b8558542cb3c2249da51a4e56b96d152
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-05-31 03:47:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 차량 정비소, Time: 393 ms, Memory: 37,280 KB -BaekjoonHub

--- SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
+++ SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
@@ -1,130 +1,130 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st1.nextToken());
-			int M = Integer.parseInt(st1.nextToken());
-			int K = Integer.parseInt(st1.nextToken());
-			int A = Integer.parseInt(st1.nextToken());
-			int B = Integer.parseInt(st1.nextToken());
-			
-			int[] shopA = new int[N+1];
-			int[] shopB = new int[M+1];
-			int[] customer = new int[K+1];
-			
-			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			for(int i = 1; i < N+1; i++) {
-				shopA[i] = Integer.parseInt(st2.nextToken());
-			}
-			
-			StringTokenizer st3 = new StringTokenizer(br.readLine());
-			for(int i = 1; i < M+1; i++) {
-				shopB[i] = Integer.parseInt(st3.nextToken());
-			}
-			
-			StringTokenizer st4 = new StringTokenizer(br.readLine());
-			for(int i = 1; i < K+1; i++) {
-				customer[i] = Integer.parseInt(st4.nextToken());
-			}
-			
-			
-			Queue<Customer> waitA = new LinkedList<>(); //a 대기
-			Queue<Customer> waitB = new LinkedList<>(); //b 대기
-			Customer[] chkA = new Customer[N+1]; //a 창구
-			Customer[] chkB = new Customer[M+1]; //b 창구
-			List<Customer> end = new ArrayList<>(); //완료 고객
-			
-			int cnt = 0;
-			int arrived = 0;
-			
-			while(true) {
-				if(cnt == K) {
-					break;
-				}
-				
 
-				for(int i = 1; i < K+1; i++) {
-					if(customer[i] == 0) {
-						waitA.add(new Customer(0, 0, i, 0, 0));
-						arrived++;
-					}
-					customer[i]--;
-				}
-				//도착 하는대로 고객 번호 부여하고, a대기열로
-				
-				
-				for(int i = 1; i < M+1; i++) {
-					if(chkB[i] == null && !waitB.isEmpty()) {
-						chkB[i] = waitB.poll();
-						chkB[i].b = i;
-					}
-					if(chkB[i] != null) {
-						chkB[i].b_time++;
-						if(chkB[i].b_time == shopB[i]) {
-							end.add(chkB[i]);
-							chkB[i] = null;
-							cnt++;
-						}
-					}
-				}
-				
-				for(int i = 1; i < N+1; i++) {
-					if(chkA[i] == null && !waitA.isEmpty()) {
-						chkA[i] = waitA.poll();
-						chkA[i].a = i;
-					}
-					if(chkA[i] != null) {
-						chkA[i].a_time++;
-						if(chkA[i].a_time == shopA[i]) {
-							waitB.add(chkA[i]);
-							chkA[i] = null;
-						}
-					}
-				}
-				
-				
-			}
-			
-			int ans = 0;
-			for(int i = 0; i < end.size(); i++) {
-				if(end.get(i).a == A && end.get(i).b == B) {
-					ans += end.get(i).c_num;
-				}
-			}
-			if(ans == 0) {
-				ans = -1;
-			}
-			System.out.println("#"+tc+" "+ans);
-			
-		}
-		
-	}
-	static class Customer{
-		int a_time;
-		int b_time;
-		int c_num;
-		int a;
-		int b;
-		
-		public Customer(int a_time, int b_time, int c_num, int a, int b) {
-			this.a_time = a_time;
-			this.b_time = b_time;
-			this.c_num = c_num;
+	static class Customer {
+		int num, aTime, bTime, a, b;
+		public Customer(int num, int aTime, int bTime, int a, int b) {
+			this.num = num;
+			this.aTime = aTime;
+			this.bTime = bTime;
 			this.a = a;
 			this.b = b;
 		}
+		@Override
+		public String toString() {
+			return "Customer [num=" + num + ", aTime=" + aTime + ", bTime=" + bTime + ", a=" + a + ", b=" + b + "]";
+		}
 	}
+	
+	static int N,M,K,A,B,answer;
+	static int[] receipt;
+	static int[] repair;
+	static int[] comeTime;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc<=T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); //접수 창구 개수
+			M = Integer.parseInt(st.nextToken()); //정비 창구 개수
+			K = Integer.parseInt(st.nextToken()); //방문한 고객의 수
+			A = Integer.parseInt(st.nextToken()); //타깃 접수 창구 번호
+			B = Integer.parseInt(st.nextToken()); //타깃 정비 창구 번호
+			
+			receipt = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i<N; i++) {
+				receipt[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			repair = new int[M];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i<M; i++) {
+				repair[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			comeTime = new int[K];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i<K; i++) {
+				comeTime[i] = Integer.parseInt(st.nextToken());
+			}
+			//입력 완료
+			
+			answer = 0;
+			Queue<Customer> waitA = new ArrayDeque<>(); //접수 창구 대기
+			Queue<Customer> waitB = new ArrayDeque<>(); //젱비 창구 대기
+			Customer[] shopA = new Customer[N]; //접수 창구
+			Customer[] shopB = new Customer[M]; //정비 창구
+			List<Customer> endList = new ArrayList<>(); //끝난 고객
+			
+			
+			while(true) {
+				//모든 고객이 다 끝나면 멈춤
+				if(endList.size() == K) {
+					break;
+				}
+				
+				//오는 대로 고객 번호 부여해서 접수 창구 대기열에 넣기
+				for(int i = 0; i<K; i++) {
+					if(comeTime[i] == 0) waitA.add(new Customer(i+1, 0, 0, 0, 0));
+					comeTime[i]--;
+				}
+				
+				//정비 창구부터
+				for(int i = 0; i<M; i++) {
+					if(shopB[i] == null && !waitB.isEmpty()) { //해당 정비 창구가 비어있고, 대기자가 있으면
+						shopB[i] = waitB.poll();
+						shopB[i].b = i+1;
+					}
+					//else if는 안됨
+					if(shopB[i] != null) { //해당 정비 창구가 비어있지 않으면
+						shopB[i].bTime++;
+						if(shopB[i].bTime == repair[i]) { //수리 완료 시
+							endList.add(shopB[i]); //끝난 고객에 넣어주고
+							shopB[i] = null; //해당 창구 비워주기
+						}
+					}
+				}
+				
+				//접수 창구
+				for(int i = 0; i<N; i++) {
+					if(shopA[i] == null && !waitA.isEmpty()) {
+						shopA[i] = waitA.poll();
+						shopA[i].a = i+1;
+					}
+					if(shopA[i] != null) {
+						shopA[i].aTime++;
+						if(shopA[i].aTime == receipt[i]) {
+							waitB.add(shopA[i]);
+							shopA[i] = null;
+						}
+					}
+				}
+			}
+			
+			int cnt = 0;
+			for(int i = 0; i<endList.size(); i++) {
+				Customer cur = endList.get(i);
+				if(cur.a == A && cur.b == B) {
+					answer += cur.num;
+					cnt++;
+				}
+			}
+			
+			if(cnt == 0) answer = -1;
+			System.out.printf("#%d %d%n", tc, answer);
+			
+		}
+	}
+
 }
