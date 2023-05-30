a80bd0b1682d00c4169b54019beb691380a83631
leunseongl <105471465+leunseongl@users.noreply.github.com>
2023-05-30 10:12:15 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 211 ms, Memory: 24,444 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -11,106 +10,102 @@ public class Solution {
 	static int[] dx = {-1,0,1,0};
 	static int[] dy = {0,-1,0,1};
 	
-	static class Data {
+	static class Core {
 		int x, y;
-		public Data(int x, int y) {
+		public Core(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 		@Override
 		public String toString() {
-			return "Data [x=" + x + ", y=" + y + "]";
+			return "Core [x=" + x + ", y=" + y + "]";
 		}
 	}
 	
-	static int N, connectCore, line;
+	static int N, core, line;
 	static int[][] map;
-	static List<Data> cores;
+	static List<Core> cores;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
 		for(int tc = 1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
+			
 			map = new int[N][N];
 			cores = new ArrayList<>();
-			
 			for(int i = 0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for(int j = 0; j<N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					if(i!=0 && i!=N-1 && j!=0 && j!=N-1) { //가장 자리가 아닌 코어 넣기
-						if(map[i][j] == 1) cores.add(new Data(i, j));
+					if(map[i][j] == 1) {
+						if(i!=0 && i!=N-1 && j!=0 && j!=N-1) cores.add(new Core(i, j));
 					}
 				}
 			}
 			//입력 완료
 			
-			connectCore = -1; //연결한 코어 수
-			line = -1; //사용한 전선 길이
-			dfs(0, 0, 0);
+			core = Integer.MIN_VALUE;
+			line = Integer.MAX_VALUE;
+			dfs(0,0,0);
 			
 			System.out.printf("#%d %d%n", tc, line);
 		}
-		
 	}
-	
-	//dfs 깊이, 연결한 코어 수, 사용한 전선 길이
+
 	private static void dfs(int cnt, int coreCnt, int length) {
 		
-		//기저 조건: 모든 코어들을 다 돌았으면
 		if(cnt == cores.size()) {
-			if(coreCnt > connectCore) { //최대한 많은 Core에 전원을 연결하였을 경우
-				connectCore = coreCnt; //최대 연결 코어 개수 갱신
-				line = length; //사용한 전선 길이도 같이 갱신
+			if(coreCnt > core) {
+				core = coreCnt;
+				line = length;
 			}
-			else if(coreCnt == connectCore) { //동일한 개수를 연결했을 경우
-				line = Math.min(line, length); //더 작은 전선 길이로 갱신
+			else if(coreCnt == core) {
+				line = Math.min(line, length);
 			}
 			return;
 		}
 		
-		Data cur = cores.get(cnt);
+		Core cur = cores.get(cnt); //현재 코어
+		//현재 코어 연결
 		for(int k = 0; k<4; k++) {
-			if(canConnect(cur, k)) { //해당 방향으로 연결 할 수 있으면
-				int ea = fill(cur, k, 2);//해당 방향대로 map에 전선 채우기
-				dfs(cnt+1, coreCnt+1, length+ea);//다음 코어 dfs 보내기
-				fill(cur, k, 0);//전선 채운거 백트래킹
+			if(canConnect(cur, k)) {
+				int tmp = fill(cur, k, 2); //전선 연결
+				dfs(cnt+1, coreCnt+1, length+tmp);
+				fill(cur, k, 0); //백트래킹
 			}
 		}
 		
-		dfs(cnt+1, coreCnt, length); //코어 연결 안하고 넘김
+		//현재 코어 연결 x
+		dfs(cnt+1, coreCnt, length);
 	}
 	
-	//해당 코어를 해당 방향으로 연결할 수 있는지 
-	private static boolean canConnect(Data core, int dir) {
-		
-		int x = core.x;
-		int y = core.y;
-		
-		while(true) {			
-			x += dx[dir];
-			y += dy[dir];
-			
-			if(x<0 || x>=N || y<0 || y>=N) break;
-			if(map[x][y] != 0) return false;
-		}
-		return true;
-	}
-	
-	//채우기
-	private static int fill(Data core, int dir, int var) {
-		int cnt = 0;
-		int x = core.x;
-		int y = core.y;
+	//그 방향으로 연결 가능한지
+	private static boolean canConnect(Core c, int dir) {
+		int x = c.x;
+		int y = c.y;
 		
 		while(true) {
 			x += dx[dir];
 			y += dy[dir];
 			
 			if(x<0 || x>=N || y<0 || y>=N) break;
+			if(map[x][y] != 0) return false; //빈공간이 아니라면 false
+		}
+		return true;
+	}
+	
+	//채우기
+	private static int fill(Core c, int dir, int var) {
+		int cnt = 0; 
+		int x = c.x;
+		int y = c.y;
+		
+		while(true) {
+			x += dx[dir];
+			y += dy[dir];
 			
+			if(x<0 || x>=N || y<0 || y>=N) break;
 			map[x][y] = var;
 			cnt++;
 		}
